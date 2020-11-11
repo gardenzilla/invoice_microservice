af70fd4fa854b1fdab290c16f8bfdc7ffb348b4a
Peter Mezei <mezeipetister@gmail.com>
2020-11-11 14:19:49 +0100

    vat multiply + test added

--- src/invoice.rs
+++ src/invoice.rs
@@ -1,4 +1,5 @@
 use chrono::*;
+use std::ops::Mul;
 
 pub struct Invoice {
     id: u32,
@@ -35,18 +36,49 @@ pub struct Customer {
 pub struct Header {}
 pub struct Item {
     name: String,
-    retail_price_net: f32,
+    retail_price_net: i32,
     vat: VAT,
-    total_price_net: f32,
-    total_price_vat: f32,
-    total_price_gross: f32,
+    total_price_net: i32,
+    total_price_vat: i32,
+    total_price_gross: i32,
 }
 
 pub enum VAT {
     AAM,
     FAD,
     TAM,
-    _0,
     _5,
+    _18,
     _27,
+}
+
+impl Mul<VAT> for i32 {
+    type Output = i32;
+
+    fn mul(self, rhs: VAT) -> Self::Output {
+        let res = match rhs {
+            VAT::AAM => self as f32 * 1.0,
+            VAT::FAD => self as f32 * 1.0,
+            VAT::TAM => self as f32 * 1.0,
+            VAT::_5 => self as f32 * 1.05,
+            VAT::_18 => self as f32 * 1.18,
+            VAT::_27 => self as f32 * 1.27,
+        };
+        res.round() as i32
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_vat_multiply() {
+        use VAT::*;
+        assert_eq!(100, 100 * AAM);
+        assert_eq!(100, 100 * FAD);
+        assert_eq!(100, 100 * TAM);
+        assert_eq!(105, 100 * _5);
+        assert_eq!(118, 100 * _18);
+        assert_eq!(127, 100 * _27);
+    }
 }
